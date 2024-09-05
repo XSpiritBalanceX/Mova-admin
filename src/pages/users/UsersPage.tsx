f953d66f45f2409055c90b064a389eb452313f60
Helen <lenagreidon@mail.ru>
2024-09-05 15:51:17 +0300

    created controls for users page

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,14 +1,25 @@
-import { Container } from "@mui/material";
+import { useState } from "react";
+import { Container, Box } from "@mui/material";
 import { translate } from "@i18n";
 import Menu from "@components/menu/Menu";
+import ControlsUsers from "@components/controlsUsers/ControlsUsers";
 import "./UsersPage.scss";
 
 const UsersPage = () => {
-  const { t } = translate("translate", { keyPrefix: "authPage" });
+  const { t } = translate("translate", { keyPrefix: "usersPage" });
+
+  const [searchWord, setSearchWord] = useState("");
+
+  const handleChangeSearch = (word: string) => {
+    setSearchWord(word);
+  };
 
   return (
     <Container className="usersPageContainer">
       <Menu />
+      <Box className="contentUsersBox">
+        <ControlsUsers searchWord={searchWord} cbHandleChangeSearch={handleChangeSearch} />
+      </Box>
     </Container>
   );
 };
