cc251c479a868c4a5c8b849aa69a0d75e84d6853
Helen <lenagreidon@mail.ru>
2024-09-05 10:37:58 +0300

    added protected routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { translate } from "@i18n";
+import { useAppSelector } from "@store/hook";
+import * as movaAdminSelectors from "@store/selectors";
+import RouterComponent from "@components/router/RouterComponent";
+import ScrollToTop from "@components/scrollToTop/ScrollToTop";
+
 const App = () => {
-  return <div>Hello</div>;
+  const locale = useAppSelector(movaAdminSelectors.localeSelect);
+  const { i18n } = translate();
+
+  useEffect(() => {
+    i18n.changeLanguage(locale);
+    // eslint-disable-next-line
+  }, [locale]);
+
+  return (
+    <>
+      <ScrollToTop />
+      <RouterComponent />
+    </>
+  );
 };
 
 export default App;
--- src/components/router/RouterComponent.tsx
+++ src/components/router/RouterComponent.tsx
@@ -1,19 +1,24 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import ProtectedRouter from "./ProtectedRouter";
+import ProtectedRouterForLogged from "./ProtectedRouterForLogged";
 
 const RouterComponent = () => {
+  //TODO: add real logic about authorization from store
+  const isLogin = true;
+
   const unauthRoutes = [{ path: "/login", element: <div>login</div> }];
 
-  const authRoutes = [{ path: "/", element: <div>main</div> }];
+  const authRoutes = [{ path: "/users", element: <div>users</div> }];
 
   return (
     <Routes>
       {unauthRoutes.map((el, ind) => (
-        <Route key={ind} path={el.path} element={el.element} />
+        <Route key={ind} path={el.path} element={<ProtectedRouterForLogged>{el.element}</ProtectedRouterForLogged>} />
       ))}
       {authRoutes.map((el, ind) => (
-        <Route key={ind} path={el.path} element={el.element} />
+        <Route key={ind} path={el.path} element={<ProtectedRouter>{el.element}</ProtectedRouter>} />
       ))}
-      <Route path="*" element={<Navigate to={"/"} />} />
+      <Route path="*" element={isLogin ? <Navigate to={"/users"} /> : <Navigate to={"/login"} />} />
     </Routes>
   );
 };
