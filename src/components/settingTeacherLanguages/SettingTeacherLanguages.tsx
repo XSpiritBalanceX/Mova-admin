e59153d95ef1629898b05100852f25845e46908c
Helen <lenagreidon@mail.ru>
2024-09-23 15:17:03 +0300

    added page with languages of teacher

--- src/components/settingTeacherLanguages/SettingTeacherLanguages.tsx
+++ src/components/settingTeacherLanguages/SettingTeacherLanguages.tsx
@@ -7,7 +7,63 @@ import * as Yup from "yup";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { useNavigate, useParams } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
+import ViewTeacherLanguages from "./ViewTeacherLanguages";
 import "./SettingTeacherLanguages.scss";
+
+export interface ITeacherLanguage {
+  id: number;
+  language: number;
+  level: number;
+  price: number;
+  description: string;
+  files: { id: number; file: string }[];
+}
+
+const mockData: ITeacherLanguage[] = [
+  {
+    id: 1,
+    language: 12,
+    level: 4,
+    price: 10,
+    description:
+      "Donec eget ornare mauris, eget interdum nulla. Praesent posuere, magna vel elementum pulvinar, sapien erat dapibus justo, quis eleifend nulla nunc ut est.",
+    files: [
+      {
+        id: 1,
+        file: "https://media.istockphoto.com/id/1453289203/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC-%D0%B8-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82.jpg?s=612x612&w=0&k=20&c=YS3prthoQV4FVhBh9ZvNXC4P_OVTxpfnrZx-cppx8-s=",
+      },
+      {
+        id: 2,
+        file: "https://media.istockphoto.com/id/1453289203/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC-%D0%B8-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82.jpg?s=612x612&w=0&k=20&c=YS3prthoQV4FVhBh9ZvNXC4P_OVTxpfnrZx-cppx8-s=",
+      },
+      {
+        id: 3,
+        file: "https://media.istockphoto.com/id/1453289203/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC-%D0%B8-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82.jpg?s=612x612&w=0&k=20&c=YS3prthoQV4FVhBh9ZvNXC4P_OVTxpfnrZx-cppx8-s=",
+      },
+      {
+        id: 4,
+        file: "https://media.istockphoto.com/id/1453289203/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC-%D0%B8-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82.jpg?s=612x612&w=0&k=20&c=YS3prthoQV4FVhBh9ZvNXC4P_OVTxpfnrZx-cppx8-s=",
+      },
+      {
+        id: 5,
+        file: "https://media.istockphoto.com/id/1453289203/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC-%D0%B8-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82.jpg?s=612x612&w=0&k=20&c=YS3prthoQV4FVhBh9ZvNXC4P_OVTxpfnrZx-cppx8-s=",
+      },
+    ],
+  },
+  {
+    id: 2,
+    language: 8,
+    level: 2,
+    price: 5,
+    description: "Donec dolor tellus, laoreet a facilisis non",
+    files: [
+      {
+        id: 1,
+        file: "https://media.istockphoto.com/id/1453289203/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC-%D0%B8-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82.jpg?s=612x612&w=0&k=20&c=YS3prthoQV4FVhBh9ZvNXC4P_OVTxpfnrZx-cppx8-s=",
+      },
+    ],
+  },
+];
 
 const SettingTeacherLanguages = () => {
   const { t } = translate("translate", { keyPrefix: "settingProfileUser" });
@@ -21,17 +77,21 @@ const SettingTeacherLanguages = () => {
 
   return (
     <Box className="languagesSettingsBox">
-      <Box className="buttonsNavigateBox">
-        {type === "languages" ? (
+      {type === "languages" && (
+        <Box className={`buttonsNavigateBox languageChangeButton`}>
           <Button type="button" name="languages_change" onClick={handleNavigate}>
             {t("change")}
           </Button>
-        ) : (
+        </Box>
+      )}
+      {type === "languages_change" && (
+        <Box className={`buttonsNavigateBox`}>
           <Button type="button" name="languages" onClick={handleNavigate}>
             <ArrowBackIosNewIcon />
           </Button>
-        )}
-      </Box>
+        </Box>
+      )}
+      {type === "languages" && <ViewTeacherLanguages languages={mockData} />}
     </Box>
   );
 };
